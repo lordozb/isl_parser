6c5c0b372a9d46e9a0884bf251d881a0b8d19740
lordozb <aadityaja@gmail.com>
2017-06-28 23:37:40 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<?php require_once("header.php"); ?>
+	<title>ISL Parser</title>
+<body>
+	
+	<div class="container">
+    	<div class="row">
+    		<div class="col-md-12 text-center">
+    			<h2 style="font-weight:bold;">Welcome to ISL Parser</h2> 
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-3"></div>
+			<div class="col-md-6">
+
+    				<label class="alert alert-info">Enter your sentence</label>
+    				<textarea name="text_input" id="text_input" class="form-control" rows="3" required="required" autofocus></textarea><br>
+	     			<button type="button" class="btn btn-success" id = "button">Parse</button>
+	   			
+    		</div>
+			<div class="col-md-3"></div>
+    	<div>
+
+    	<div class = "row">
+    		<div class = "col-md-12">
+    			<table class="table table-striped" id = "table">
+    				<thead>
+    					<tr>
+    						<th>Word</th>
+    						<th>Tags</th>
+    						<th>Root</th>
+    					</tr>
+    				</thead>
+    				<tbody id = "tbody">
+
+    				</tbody>
+    			</table>
+    		</div>
+
+    	</div>
+    </div>  
+
+
+    <?php require_once("footer.php"); ?>
+
+    <script>
+    	
+    	$("document").ready(function(){
+    		$("#button").click(function(){
+    			var value = $('textarea#text_input').val();
+    			var array = {};
+    			array["key"] = value;
+    			$.post("parse.php",array,function(data, status){
+    				s = JSON.parse(data);
+    				console.log(s);
+					$("#tbody").empty();
+					 $.each(s, function(key, val){
+					 	var tr = $('<tr></tr>');
+					 	$('<td>'+val[0]+'</td> '+' <td>'+val[1]+'</td>').appendTo(tr);
+					 	tr.appendTo('#tbody');
+					 });  							
+    				
+       			});
+    		});
+    	});
+    		  		
+
+    </script>
+</body>
+<html>
---
+++ parse.php
@@ -0,0 +1,11 @@
+<?php
+
+	include("/var/www/html/isl_parser/PHP-Stanford-NLP/autoload.php");
+
+	$parser = new \StanfordNLP\Parser(
+  '/var/www/html/isl_parser/stanford-parser-full-2017-06-09/stanford-parser.jar',
+  '/var/www/html/isl_parser/stanford-parser-full-2017-06-09/stanford-parser-3.8.0-models.jar'
+);
+	$result = $parser->parseSentence($_POST["key"]);
+	echo json_encode($result["wordsAndTags"]);
+?>
